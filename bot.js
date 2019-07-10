6447933fbce0f23c7f2cff55d37dc9b3af985698
nastogmr <51057724+nastogmr@users.noreply.github.com>
2019-07-10 12:27:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,63 +1,68 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('message', async message => {
- 
-   if (message.content.startsWith("_new")) {  
-        const reason = message.content.split(" ").slice(1).join(" ");  
-        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`Support Team\` وتنطي البوت ادمنيتر حتا يقدر يسوي الرومات ويعدل برمشنات`);
-        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
-        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
-            let role = message.guild.roles.find("name", "Support Team");
-            let role2 = message.guild.roles.find("name", "@everyone");
-            c.overwritePermissions(role, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });  
-            c.overwritePermissions(role2, {
-                SEND_MESSAGES: false,
-                READ_MESSAGES: false
-            });
-            c.overwritePermissions(message.author, {
-                SEND_MESSAGES: true,
-                READ_MESSAGES: true
-            });
-            message.channel.send(`:white_check_mark: تم انشاء تذكرتك, #${c.name}.`);
-            const embed = new Discord.RichEmbed()
-                .setColor(d1631e)
-                .addField(`Hey ${message.author.username}!`, `:white_check_mark:  تم انشاء تذكرتك, #ticket`)
-                .setTimestamp();
-            c.send({
-                embed: embed
-            });
-        }).catch(console.error);
-    }
- 
- 
-  if (message.content.startsWith("_close")) {
-        if (!message.channel.name.startsWith(`_ticket`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
- 
-       message.channel.send(`هل انت متأكد من اقفالك للتذكرة اذا متأكد اكتبconfirm`)
-           .then((m) => {
-               message.channel.awaitMessages(response => response.content === 'confirm', {
-                       max: 1,
-                       time: 10000,
-                       errors: ['time'],
-                   })  
-                   .then((collected) => {
-                       message.channel.delete();
-                   })  
-                   .catch(() => {
-                       m.edit('لم يتم تقفيل التكت').then(m2 => {
-                           m2.delete();
-                       }, 3000);
-                   });
-           });
-   }
- 
-});
 var prefix = "_"
-
-
+ 
+client.on('message', message => {
+    var p = message.mentions.members.first();
+    var reason = message.content.split(" ").slice(2).join(' ');
+    var log = message.guild.channels.find('name', 'warns-log');
+    if(message.content.startsWith(`${prefix}warn`)){
+        if(!p) return message.reply(`**منشن الشخص اول**`);
+        if(!reason) return message.reply(`**حط سبب**`);
+        if(!p.bannable) return message.reply(`**مقدر اعطي وورن لشخص من الادارة**`);
+        reason = reason.replace('1', "**كتابة الاوامر بالشات العام**");
+        reason = reason.replace('2', "**بيع اشياء**");
+        reason = reason.replace('3', "**التحذث عن السياسة**");
+        reason = reason.replace('4', "**التحذث عن الدين **");
+        reason = reason.replace('5', "**التحدث عن الطائفية**");
+        reason = reason.replace('6', "**السبام**");
+        reason = reason.replace('7', "**فتح تذكرة من دون سبب**");
+        reason = reason.replace('8', "**العنصرية**");
+        reason = reason.replace('9', "**عدم الاحترام**");
+        reason = reason.replace('10', "**نشر بالعام**");
+        var embed = new Discord.RichEmbed()
+        .setAuthor(`تم التحذير`)
+        .addField(`Name ♣`, `<@${p.id}>`)
+        .addField(`By ♣`, `<@${message.author.id}>`)
+        .addField(`Reason ♣`, reason)
+        .setTimestamp()
+        .setColor("WHITE")
+        .setFooter(` `)
+        message.channel.send(`${p} ${reason}`)
+            message.delete();
+        log.send({embed});
+        warnRoles = ['Only Me']
+    }
+});
+///////////
+client.on('message', message => {
+    var p = message.mentions.members.first();
+    var reason = message.content.split(" ").slice(2).join(' ');
+    var log = message.guild.channels.find('name', 'ban-log');
+    if(message.content.startsWith(`${prefix}ban`)){
+        if(!p) return message.reply(`**منشن الشخص**`);
+        if(!reason) return message.reply(`**حط سبب**`);
+        if(!p.bannable) return message.reply(`**م اقدر ابتد شخص من الستاف**`);
+        reason = reason.replace('1', "**نشر في الخاص**");
+        reason = reason.replace('2', "**اسم غير لائق**");
+        reason = reason.replace('3', "**صوره غير لائقه**");
+        reason = reason.replace('4', "**اسم غير لآئق**");
+        reason = reason.replace('5', "**سب الاهل**");
+        var embed = new Discord.RichEmbed()
+        .setAuthor(`User Banned!`)
+        .addField(`Name ♣`, `<@${p.id}>`)
+        .addField(`By ♣`, `<@${message.author.id}>`)
+        .addField(`Reason ♣`, reason)
+        .setTimestamp()
+        .setColor("BLACK")
+        .setFooter(` `)
+        p.ban();
+            message.delete();
+        log.send({embed});
+        banRoles = ['Only Me']
+    }
+});
+///////////////////////////
 client.on('message',async message => {
   var room;
   var title;
@@ -128,7 +133,201 @@ client.on('message',async message => {
     });
   }
 });
-
+////////
+client.on('message', async message => {
+ 
+if(message.content.startsWith( prefix + 'invite')) {
+        let oi = message.mentions.users.first() ? message.mentions.users.first().id : message.author.id;
+        let Tag = message.mentions.users.first() ? message.mentions.users.first().tag : message.author.tag;
+        let Username = message.mentions.users.first() ? message.mentions.users.first().username : message.author.username;
+        let Avatar = message.mentions.users.first() ? message.mentions.users.first().avatarURL : message.author.avatarURL;
+       
+        message.guild.fetchInvites().then(invs => {
+            let member = client.guilds.get(message.guild.id).members.get(oi);
+            let personalInvites = invs.filter(i => i.inviter.id === oi);
+            let urll = invs.filter(i => i.inviter.id === oi);
+            let link = urll.reduce((p , v) => v.url +` , Total de membros recrutados no convite: ${v.uses}.\n`+ p, `\nServidor: ${message.guild.name} \n `);
+            let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+            let inviteCode = personalInvites.reduce((p, v) => v.code);
+            let possibleInvites = [['Total de membros recrutados:']];
+            possibleInvites.push([inviteCount, inviteCode]);
+            let user = message.mentions.users.first() || message.author;
+            let mem = message.guild.member(user);
+            let millisJoined = new Date().getTime() - mem.joinedAt.getTime();
+            let daysJoined = millisJoined / 1000 / 60 / 60 / 24;
+           
+            var inviteInfo = new Discord.RichEmbed()
+            .setTitle(`:incoming_envelope: **[INVITE INFO]** ${Username}`)
+            .setThumbnail(client.user.avatarURL)
+            .addField('**الدعوات**', `**➥** [ شخص **${Number(inviteCount)}** ]`)
+            .addField('**تم الانضمام للسيرفر من**', `**➥** [ يوم **${daysJoined.toFixed(0)}** ]`)
+            .addField('**رابط دعوة الانضمام**', `**➥** [ **https://discord.gg/${inviteCode || 'Zm2U6we'}** ]`)
+            .setColor('ORANGE')
+            .setTimestamp()
+            .setFooter(Tag, Avatar)
+           
+            message.channel.send(inviteInfo);
+            });
+    };
+});
+//////////////////////////////////
+var config = {
+  events: [
+    {type: "CHANNEL_CREATE", logType: "CHANNEL_CREATE", limit: 4 , delay: 5000},
+    {type: "CHANNEL_DELETE", logType: "CHANNEL_DELETE", limit: 4, delay: 5000},
+    {type: "GUILD_MEMBER_REMOVE", logType: "MEMBER_KICK", limit: 4, delay: 5000},
+    {type: "GUILD_BAN_ADD", logType: "MEMBER_BAN_ADD", limit: 4, delay: 5000},
+    {type: "GUILD_ROLE_CREATE", logType: "ROLE_CREATE", limit: 5, delay: 5000},
+    {type: "GUILD_ROLE_DELETE", logType: "ROLE_DELETE", limit: 4, delay: 5000},
+  ]
+}
+client.on("error", (e) => console.error(e));
+client.on("raw", (packet)=> {
+  let {t, d} = packet, type = t, {guild_id} = data = d || {};
+  if (type === "READY") {
+    client.startedTimestamp = new Date().getTime();
+    client.captures = [];
+  }
+  let event = config.events.find(anEvent => anEvent.type === type);
+  if (!event) return;
+  let guild = client.guilds.get(guild_id);
+  if (!guild) return;
+  guild.fetchAuditLogs({limit : 1, type: event.logType})
+    .then(eventAudit => {
+      let eventLog = eventAudit.entries.first();
+      if (!eventLog) return;
+      let executor = eventLog.executor;
+      guild.fetchAuditLogs({type: event.logType, user: executor})
+        .then((userAudit, index) => {
+          let uses = 0;
+          userAudit.entries.map(entry => {
+            if (entry.createdTimestamp > client.startedTimestamp && !client.captures.includes(index)) uses += 1;
+          });
+          setTimeout(() => {
+            client.captures[index] = index
+          }, event.delay || 2000)
+          if (uses >= event.limit) {
+            client.emit("reachLimit", {
+              user: userAudit.entries.first().executor,
+              member: guild.members.get(executor.id),
+              guild: guild,
+              type: event.type,
+            })
+          }
+        }).catch(console.error)
+    }).catch(console.error)
+});
+client.on("reachLimit", (limit)=> {
+  let log = limit.guild.channels.find( channel => channel.name === "security-log");
+  log.send(limit.user.username+"\** سيرفر بيتهكر ! ** ");
+  limit.guild.owner.send(limit.user.username+"\** سيرفرك بيتهكر ! ** ")
+  limit.member.roles.map(role => {
+    limit.member.removeRole(role.id)
+    .catch(log.send)
+  });
+});
+///////
+client.on('message', message => {
+    if(message.content.startsWith(prefix + 'new')) {
+        let args = message.content.split(' ').slice(1).join(' ');
+        let support = message.guild.roles.find("name","Support Team");
+        let ticketsStation = message.guild.channels.find("name", "TICKETS.");
+        if(!args) {
+            return message.channel.send('**المرجو كتآبة موضوع للتذكرة**');
+        };
+                if(!support) {
+                    return message.channel.send('** من فضلك قم بإنشاء رتبة اسمها `Support Team` **');
+                };
+            if(!ticketsStation) {
+                message.guild.createChannel("TICKETS.", "category");
+            };
+                message.guild.createChannel(`ticket-${message.author.username}`, "text").then(ticket => {
+                    message.delete()
+                        message.channel.send(`Your ticket has been created. [ ${ticket} ]`);
+                    ticket.setParent(ticketsStation);
+                    ticketsStation.setPosition(1);
+                        ticket.overwritePermissions(message.guild.id, {
+                            SEND_MESSAGES: false,
+                            READ_MESSAGES: false
+                        });
+                            ticket.overwritePermissions(support.id, {
+                                SEND_MESSAGES: true,
+                                READ_MESSAGES: true
+                            });
+                                ticket.overwritePermissions(message.author.id, {
+                                    SEND_MESSAGES: true,
+                                    READ_MESSAGES: true
+                                });
+                    let embed = new Discord.RichEmbed()
+                                .setTitle('**New Ticket.**')
+                                .setColor("RANDOM")
+                                .setThumbnail(`${message.author.avatarURL}`)
+                                .addField('Subject', args)
+                                .addField('Author', message.author)
+                                .addField('Channel', `<#${message.channel.id}>`);
+ 
+                                ticket.sendEmbed(embed);
+                }) .catch();
+    }
+    if(message.content.startsWith(prefix + 'close')) {
+            if(!message.member.hasPermission("ADMINISTRATOR")) return;
+        if(!message.channel.name.startsWith("ticket")) {
+            return;
+        };  
+                let embed = new Discord.RichEmbed()
+                    .setAuthor("أعد الامر ، لديك 20 ثآنية")
+                    .setColor("RANDOM");
+                    message.channel.sendEmbed(embed) .then(codes => {
+ 
+                   
+                        const filter = msg => msg.content.startsWith(prefix + 'close');
+                        message.channel.awaitMessages(response => response.content === prefix + 'close', {
+                            max: 1,
+                            time: 20000,
+                            errors: ['time']
+                        })
+                        .then((collect) => {
+                            message.channel.delete();
+                        }) .catch(() => {
+                            codes.delete()
+                                .then(message.channel.send('**تم إلغاء العملية**')) .then((c) => {
+                                    c.delete(4000);
+                                })
+                                   
+                           
+                        })
  
  
-client.login("process.env.BOT_TOKEN");
+                    })
+ 
+ 
+           
+    }
+});
+//////
+client.on("guildMemberAdd", member => {
+  client.channels.find('id', '582093431770185745').send(` **Welcome To .. Server**  `)
+});
+/////////
+client.on('message', msg => {
+    if(msg.content === '$help')
+    msg.reply('Check Your DM :white_check_mark:')
+  });
+ 
+ 
+  client.on("message", message => {
+    if (message.content === "$help") {
+     const embed = new Discord.RichEmbed()
+         .setColor("#00FF00")
+         .setThumbnail(message.author.avatarURL)
+         .setDescription(`**Help|هيلب
+       $invites | لمعرفة عدد انفايتاتك
+       $new | لإنشاء تكت
+       $giveaway  |  لإعداد قيفاواي
+       ** `)
+   message.author.sendEmbed(embed)
+   
+   }
+   });
+/////
+client.login(process.env.BOT_TOKEN);
